0f330018de9c9913eda9e4c11c5afafabe11e288
Vikas V Shastry <vikasvshastry@gmail.com>
2017-12-13 20:36:30 +0530

    added fragments.

--- app/src/main/java/com/chatterbox/chatterbox/MainActivity.java
+++ app/src/main/java/com/chatterbox/chatterbox/MainActivity.java
@@ -1,6 +1,9 @@
 package com.chatterbox.chatterbox;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +12,8 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private int fragno;
+    Fragment fragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,41 @@ public class MainActivity extends AppCompatActivity {
         }
 
         setContentView(R.layout.activity_main);
+
+        displayView(0);
+
+    }
+
+    private void displayView(int position) {
+        String title = getString(R.string.app_name);
+        fragno=position;
+        switch (position) {
+            case 0:
+                fragment = new chatsFragment();
+                title = "ChatterBox";
+                break;
+            case 1:
+                fragment = new messagesFragment();
+                title = "messages";
+                break;
+        }
+
+        if (fragment != null) {
+            String backStateName =  fragment.getClass().getName();
+
+            FragmentManager manager = getSupportFragmentManager();
+            boolean fragmentPopped = manager.popBackStackImmediate (backStateName, 0);
+
+            if (!fragmentPopped && manager.findFragmentByTag(backStateName) == null){ //fragment not in back stack, create it.
+                FragmentTransaction ft = manager.beginTransaction();
+                ft.replace(R.id.container_body, fragment, backStateName);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack(backStateName);
+                ft.commit();
+            }
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
     }
 
 }
---
+++ app/src/main/java/com/chatterbox/chatterbox/chatsFragment.java
@@ -0,0 +1,41 @@
+package com.chatterbox.chatterbox;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class chatsFragment extends Fragment {
+
+    public chatsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_chats, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
